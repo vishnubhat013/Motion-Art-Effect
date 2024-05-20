ae749598c718618610e6a54fb362b8b23c7519a6
vishnubhat013 <vishnubhat357@gmail.com>
2024-05-20 23:24:25 +0530

    button click done

--- src/components/Homeone.jsx
+++ src/components/Homeone.jsx
@@ -3,24 +3,26 @@ import { GoArrowRight } from "react-icons/go";
 
 function Homeone() {
     const handleClick = () => {
-        window.location.href = 'https://codecanyon.net/checkout/102316867/create_account?_ga=2.43125925.1669195575.1716209776-1129150054.1713500484';
+        const url = 'https://codecanyon.net/checkout/102316867/create_account?_ga=2.43125925.1669195575.1716209776-1129150054.1713500484';
+        
+        window.open(url,'noopener,noreferrer');
       };
   return (
 <>
     <div className='flex flex-col w-[90vw] md:w-[85vw] mx-auto pb-8 mb-20'>
 
-    <div className='pt-12 mx-8'>
-      <div className=" px-4 pt-10 flex justify-between">
+    <div className='pt-12 md:mx-8 mx--'>
+      <div className=" md:px-4 px-0 pt-10 flex justify-between">
       <div>
-        <img src="/MotionArtEffect-logo.png" alt="motion art effect"/>
+        <img src="/MotionArtEffect-logo.png" alt="motion art effect" className="md:justify-start"/>
         </div>
         <div>
-      <button type="button" className =" md:block hidden justify-end text-[#0d051f] bg-[#ffffff] font-medium rounded-md text-lg px-8 py-3 me-2 mb-2 ">Purchase Now</button>
+      <button type="button" onClick={handleClick}className =" md:block hidden justify-end text-[#0d051f] bg-[#ffffff] font-medium rounded-md text-lg px-8 py-3 me-2 mb-2 ">Purchase Now</button>
       </div>
       </div>
 
 
-      <div className="flex mt-8 md:flex-row flex-col justify-center items-start w-full">
+      <div className="flex mt-8 md:flex-row flex-col  items-center md:items-start w-full">
         <div className="bg-gradient-to-r from-[#f87516] to-[55%] font-semibold to-[#5e11ff] px-6 pt-8 flex-col w-fit bg-clip-text md:justify-start md:items-start h-full items-center text-xl md:text-left text-center">
         <p className="text-transparent">Transform</p>
         <p className='text-transparent'>Your Website</p>
@@ -28,13 +30,13 @@ function Homeone() {
         <p className='text-white'>Effect</p>
         </div>
         <div className="bg-gradient-to-r from-[#f87516] to-[70%] font-semibold to-[#5e11ff] md:px-40  px-2 pt-10 flex-col w-fit  items-center bg-clip-text text-xl text-left">
-        <p className="text-white md:text-7xl text-4xl  ">Attract Your</p>
-        <p className='text-white md:text-7xl text-4xl py-2'>Visitors Attention</p>
-        <p  className='text-white pt-2 md:text-7xl text-4xl py-2'>With Colourfull</p>
-        <p className="text-transparent md:text-7xl text-4xl py-2'"> Motion Art Effect</p>
-        <p className="text-[#9e99ae]  text-sm  md:text-lg pt-8">Unleash the power of creativity with Motion Art for Elementor - your </p>
-        <p className="text-[#9e99ae] text-sm  md:text-lg">ultimate solution for seamlessly integrating captivity animantions into </p>
-        <p className="text-[#9e99ae] text-sm  md:text-lg">your websites.</p>
+        <p className="text-white md:text-7xl text-4xl text-center  ">Attract Your</p>
+        <p className='text-white md:text-7xl text-4xl py-2 text-center'>Visitors Attention</p>
+        <p  className='text-white pt-2 md:text-7xl text-4xl text-center py-2'>With Colourfull</p>
+        <p className="text-transparent md:text-7xl text-4xl py-2 text-center"> Motion Art Effect</p>
+        <p className="text-[#9e99ae]  text-sm  md:text-lg text-center pt-8">Unleash the power of creativity with Motion Art for Elementor - your </p>
+        <p className="text-[#9e99ae] text-sm  md:text-lg text-center">ultimate solution for seamlessly integrating captivity animantions into </p>
+        <p className="text-[#9e99ae] text-sm  md:text-lg text-center">your websites.</p>
         </div>
       </div>
 
@@ -62,18 +64,18 @@ function Homeone() {
         </div>
     </div>
     
-<div className="flex mt-28 text-2xl gap-8 justify-center items-center md:flex-row flex-col">
-    <div>
-    <p className="text-[#eee5ff] text-3xl md:text-5xl  font-semibold text-center">Turn Your Cursor into colourful</p>
-    <p className="text-[#eee5ff]  text-3xl md:text-5xl text-center font-semibold ">Magic Wand & Charm your Visitor</p>
-    <p className="text-[#b3acc5] md:text-base text-xs pt-6 text-center">Motion Art for Elementor is a groundbreaking plugin that empowers you to effortlessly infuse</p>
-    <p className=" text-[#b3acc5] md:text-base text-xs pt-1 text-center ">your website with visually stunning motion art elements.</p>
+<div className="flex mt-28 text-2xl gap-8 justify-center  items-center md:flex-row flex-col">
+    <div>  
+    <p className="text-[#eee5ff] text-3xl md:text-5xl  font-semibold md:text-start text-center">Turn Your Cursor into colourful</p>
+    <p className="text-[#eee5ff]  text-3xl md:text-5xl text-center md:text-start font-semibold ">Magic Wand & Charm your Visitor</p>
+    <p className="text-[#b3acc5] md:text-lg text-base pt-6 md:text-start text-center">Motion Art for Elementor is a groundbreaking plugin that empowers you to effortlessly infuse</p>
+    <p className=" text-[#b3acc5] md:text-lg text-base pt-1 md:text-start text-center ">your website with visually stunning motion art elements.</p>
      <div className="mt-8 text-red-100">
-    <button className="bg-gradient-to-r from-[#5e11ff] to-[110%] to-[#f87516] rounded-lg text-lg px-14 py-4 flex items-center gap-4" onclick={handleClick}>Purchase From Envato<GoArrowRight size={25} c/></button>
+    <button type="button" className="bg-gradient-to-r from-[#5e11ff] to-[110%] to-[#f87516] rounded-2xl text-2xl px-14 py-4 flex items-center gap-4" onClick={handleClick}>Purchase From Envato<GoArrowRight size={25} c/></button>
 </div>
     </div>
     <div className="md:items-end items-center">
-        <img src="/motionarteffect-img5.png "/>
+        <img src="/motionarteffect-img5.png " className=""/>
     </div>
 </div>
 
@@ -122,9 +124,9 @@ function Homeone() {
     <div className="md:mt-32 mt-8 flex-col justify-center text-center">
         <p className="text-[#eee5ff] md:text-4xl  text-3xl flex text-center justify-center font-semibold">An All-Rounded Plugin with</p>
         <p  className="text-[#eee5ff] md:text-4xl text-3xl flex justify-center font-semibold">Powerful Features</p>
-        <p className="text-[#9890a9] pt-6 flex justify-center items-center ">Whether you're a seasoned web designer or just starting out,Motion Art for</p>
-        <p className="text-[#9890a9] pt-1 flex justify-center">Elementor seamlessly integrates with the Elementor platform, providing you </p>
-        <p className="text-[#9890a9] pt-1 flex justify-center">with a seamless and intuitive experience.</p>
+        <p className="text-[#9890a9] pt-6 flex md:text-lg text-base justify-center items-center ">Whether you're a seasoned web designer or just starting out,Motion Art for</p>
+        <p className="text-[#9890a9] pt-1 flex justify-center md:text-lg text-base">Elementor seamlessly integrates with the Elementor platform, providing you </p>
+        <p className="text-[#9890a9] pt-1 flex justify-center md:text-lg text-base">with a seamless and intuitive experience.</p>
 
     </div>
 </div>
@@ -134,25 +136,25 @@ function Homeone() {
         <div className=" justify-between bg-gradient-to-b from-[#0d061f] to-[#251e35]  rounded-3xl ">
         <img src="/motionarteffect-img9.png" alt="thunder"/>
         <div className="px-8 py-4">
-        <p className="text-[#eee5ff] text-3xl pt-4 font-semibold">Light Weight</p>
-        <p className="text-[#9890a9] pt-6 flex ">Motion Art for Elementor is designed to be</p>
-        <p className="text-[#9890a9] pt-1 flex ">lightweight.</p>
+        <p className="text-[#eee5ff] text-2xl pt-4 font-semibold">Light Weight</p>
+        <p className="text-[#9890a9] pt-6 flex ">Motion Art for Elementor is designed to be lightweight.</p>
+
         </div>
         </div>
         <div className=" justify-between bg-gradient-to-b from-[#0d061f] to-[#251e35] rounded-3xl">
         <img src="/motionarteffect-img6.png" alt="thunder"/>
         <div className="px-8 py-4">
-        <p className="text-[#eee5ff] text-3xl pt-4 font-semibold">100% Responsive</p>
-        <p className="text-[#9890a9] pt-6 flex">Create a constitent visual experience</p>
-        <p className="text-[#9890a9] pt-1 flex ">across all devices.</p>
+        <p className="text-[#eee5ff] text-2xl pt-4 font-semibold">100% Responsive</p>
+        <p className="text-[#9890a9] pt-6 flex">Create a constitent visual experience across all devices.</p>
+        
         </div>
         </div>
         <div className=" justify-between bg-gradient-to-b from-[#0d061f] to-[#251e35] rounded-3xl">
         <img src="/motionarteffect-img7.png" alt="thunder"/>
         <div className="px-8 py-4">
-        <p className="text-[#eee5ff] text-3xl pt-4 font-semibold">User Friendly Interface</p>
-        <p className="text-[#9890a9] pt-6 flex ">Ensure a smooth experience for both</p>
-        <p className="text-[#9890a9] pt-1 flex ">applications and administrators.</p>
+        <p className="text-[#eee5ff] text-2xl pt-4 font-semibold">User Friendly Interface</p>
+        <p className="text-[#9890a9] pt-6 flex ">Ensure a smooth experience for both applications and administrators.</p>
+    
         </div>
         </div>
     </div>
