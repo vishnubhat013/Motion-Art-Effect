9363c9edf157fb6ffa4400e3ea48d79e19e461e7
vishnubhat013 <vishnubhat357@gmail.com>
2024-05-20 20:52:07 +0530

    Background animation

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import Header from './components/Header';
 import './App.css';
 import Homeone from './components/Homeone';
 import Footer from './components/Footer';
-
+import Background from './components/Background';
 function App() {
+
   return (
     <>
-    <div className='flex flex-col'>
+    {/* <div className='flex flex-col'>
     <Header/>
     <Homeone/>
     <Footer/>
-    </div>
+    </div> */}
+    <Background />
     </>
   );
 }
---
+++ src/components/Background.js
@@ -0,0 +1,20 @@
+import {useEffect} from 'react';
+export default function Background(){
+    useEffect(() => {
+        const script = document.createElement('script');
+    
+        script.src = "/script.js";
+        script.async = true;
+    
+        document.body.appendChild(script);
+    
+        return () => {
+          document.body.removeChild(script);
+        }
+      }, []);
+    return(
+        <>
+            <canvas></canvas>
+        </>
+    )
+}
